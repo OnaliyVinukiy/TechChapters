0ddce51c823784dee79f486d763f6a67c51a2c02
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-06-03 17:53:30 +0530

    firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "nchapters-35c6b.firebaseapp.com",
-  projectId: "nchapters-35c6b",
-  storageBucket: "nchapters-35c6b.appspot.com",
-  messagingSenderId: "828195514420",
-  appId: "1:828195514420:web:3768c278853e80c3230261",
-  databaseURL: "https://nchapters-af71f-default-rtdb.firebaseio.com/"
+  apiKey: "AIzaSyCzGZjohQaxi6N0e4nHQmxAgL40bxUto30",
+  authDomain: "techchapters-a381e.firebaseapp.com",
+  databaseURL: "https://techchapters-a381e-default-rtdb.firebaseio.com",
+  projectId: "techchapters-a381e",
+  storageBucket: "techchapters-a381e.appspot.com",
+  messagingSenderId: "350355077376",
+  appId: "1:350355077376:web:53059c1623aeeff2a65be5",
+  measurementId: "G-ENHLKCGHGW"
 };
-
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
