3434204ffc39cf26a7a0ddafd755468cacc6a71f
OnaliyVinukiy <ovjayawardana@students.nsbm.ac.lk>
2024-08-17 21:59:57 +0530

    google auth

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCzGZjohQaxi6N0e4nHQmxAgL40bxUto30",
   authDomain: "techchapters-a381e.firebaseapp.com",
@@ -20,3 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { auth, database };
+export const googleProvider = new GoogleAuthProvider();
