1b02c154d77f30b843c6012bf7c72648eb2769dc
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-08-19 03:30:15 +0530

    subscription plans frontend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -497,7 +497,7 @@ const Header = () => {
                   >
                     <li>
                       <a
-                        href="/Religous"
+                        href="/Subscriptions"
                         class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                       >
                         Subscription
---
+++ client/src/components/Subscriptions.jsx
@@ -0,0 +1,328 @@
+import React from "react";
+
+function Subscriptions() {
+  return (
+    <div className="flex flex-wrap justify-around mt-24">
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+          Basic plan
+        </h5>
+        <div className="flex items-baseline text-gray-900 dark:text-white">
+          <span className="text-3xl font-semibold"></span>
+          <span className="text-5xl font-extrabold tracking-tight">Free</span>
+          <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+           
+          </span>
+        </div>
+        <ul role="list" className="space-y-5 my-7">
+          <li className="flex items-center">
+            <svg
+              className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+              One event per month
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+              Upto 5 event feedback
+            </span>
+          </li>
+         
+          <li class="flex line-through decoration-gray-500">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 ms-3">
+              Participant registration for events
+            </span>
+          </li>
+          <li class="flex line-through decoration-gray-500">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 ms-3">
+              Volunteer registration for events
+            </span>
+          </li>
+          <li class="flex line-through decoration-gray-500">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 ms-3">
+              Email notifications
+            </span>
+          </li>
+          <li class="flex line-through decoration-gray-500">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 ms-3">
+              Goolge calendar notifications
+            </span>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+        >
+          Choose plan
+        </button>
+      </div>
+
+     
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+          Pro plan
+        </h5>
+        <div className="flex items-baseline text-gray-900 dark:text-white">
+          <span className="text-3xl font-semibold">$</span>
+          <span className="text-5xl font-extrabold tracking-tight">5</span>
+          <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+            /month
+          </span>
+        </div>
+        <ul role="list" className="space-y-5 my-7">
+          <li className="flex items-center">
+            <svg
+              className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            5 events per month
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+              Unlimited event feedback
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            Participant registration for events
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            Volunteer registration for events
+            </span>
+          </li>
+          <li class="flex line-through decoration-gray-500">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 ms-3">
+            Email notifications
+            </span>
+          </li>
+          <li class="flex line-through decoration-gray-500">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-gray-400 dark:text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 ms-3">
+            Goolge calendar notifications
+            </span>
+          </li>
+          
+        </ul>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+        >
+          Choose plan
+        </button>
+      </div>
+
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
+          Premium plan
+        </h5>
+        <div className="flex items-baseline text-gray-900 dark:text-white">
+          <span className="text-3xl font-semibold">$</span>
+          <span className="text-5xl font-extrabold tracking-tight">10</span>
+          <span className="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400">
+            /month
+          </span>
+        </div>
+        <ul role="list" className="space-y-5 my-7">
+          <li className="flex items-center">
+            <svg
+              className="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+              Unlimited events
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+              Unlimited event feedback
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            Participant registration for events
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            Volunteer registration for events
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            Email notifications
+            </span>
+          </li>
+          <li class="flex">
+            <svg
+              class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+            </svg>
+            <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
+            Goolge calendar notifications
+            </span>
+          </li>
+          
+        </ul>
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
+        >
+          Choose plan
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Subscriptions;
