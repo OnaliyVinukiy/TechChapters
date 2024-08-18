ca4c201db15b0b8b22137edda69d534f6e1270f4
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-08-18 13:49:25 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
             className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
           >
             <h1 className="flex flex-wrap text-xl font-bold cursor-pointer sm:text-2xl md:text-3xl">
-              <span className="text-green-600">N</span>
+              <span className="text-green-600">Tech</span>
               <span className="text-blue-800">Chapters</span>
             </h1>
           </a>
@@ -44,7 +44,7 @@ export default function Footer() {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-700">
             © 2024{" "}
             <a href="#" className="hover:underline">
-              NChapters
+              TechChapters
             </a>
             . All Rights Reserved.
           </span>
