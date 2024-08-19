c6056d92e7e56e683f814bb69d422b84d5102c8d
Jayamuni Rashminda <rashminda121@gmail.com>
2024-08-19 09:40:31 +0530

    payment

--- client/src/components/payment.jsx
+++ client/src/components/payment.jsx
@@ -1,7 +1,20 @@
 import s1 from "../images/sample/9.jpg";
 import s2 from "../images/sample/10.jpg";
 
+import axios from "axios";
+import { loadStripe } from "@stripe/stripe-js/pure";
+
 const Payment = () => {
+  const onClick = async () => {
+    const stripe = await loadStripe(
+      "pk_test_51Pp8JZDJE9QH0zh48YRGhD7dSKgpxuiH5DqwCxc0mBoQ6SCkd9DBG5HfAYUlOcIX6jWQ9aJsRMeSzNvAv6CiAyhr00q1tPlPID"
+    );
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+  };
+
   return (
     <div className="mt-20 pt-10 flex flex-col items-center md:flex-row md:justify-center md:space-x-6 space-y-6 md:space-y-0">
       <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl hover:scale-105 transition-transform">
@@ -25,12 +38,12 @@ const Payment = () => {
             strong security measures. Get started today and enjoy a smooth
             payment experience.
           </p>
-          <a
-            href="#"
+          <button
+            onClick={onClick}
             className="flex w-full items-center justify-center px-3 py-2 text-sm font-medium text-white bg-green-600 hover:bg-green-800  rounded-lg  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Continue
-          </a>
+          </button>
         </div>
       </div>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl hover:scale-105 transition-transform">
